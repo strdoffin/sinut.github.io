ed951014662cea8f8390b3d491244a23ac3b68e7
strdoffin <undefu5624@gmail.com>
2023-05-25 12:56:48 +0700

    unzip file

---
+++ NCDs-WEBpage/progress.js
@@ -0,0 +1,10 @@
+function bar(){
+  let perProgress_1 = document.getElementById('progress_1');
+  let perProgress_2 = document.getElementById('progress_2');
+  let perProgress_3 = document.getElementById('progress_3');
+  let perProgress_4 = document.getElementById('progress_4');
+  let perProgress_5 = document.getElementById('progress_5');
+
+  perProgress_1.style.width = '50%';
+
+}
